6df1bf7ddad4a0d0e33f821304c870db109c9ed1
Peli <peli0101@gmail.com>
2011-06-04 21:48:12 +0000

    OI Shopping List: Fix crash in Honeycomb by using correct compatibility layer.
    git-svn-id: https://openintents.googlecode.com/svn/trunk@3387 72b678ce-9140-0410-bee8-679b907dd61a

--- ShoppingList/src/org/openintents/shopping/ShoppingListFilterOS3.java
+++ ShoppingList/src/org/openintents/shopping/ShoppingListFilterOS3.java
@@ -16,7 +16,7 @@
 package org.openintents.shopping;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v3.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
