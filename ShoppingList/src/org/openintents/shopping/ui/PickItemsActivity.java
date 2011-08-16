153d9fff0317f265dde4e4f6b441c3f942157b91
Peli <peli0101@gmail.com>
2011-08-16 22:34:53 +0000

    OI Shopping List: android:onClick is not yet supported in Cupcake.
    git-svn-id: https://openintents.googlecode.com/svn/trunk@3597 72b678ce-9140-0410-bee8-679b907dd61a

--- ShoppingList/src/org/openintents/shopping/ui/PickItemsActivity.java
+++ ShoppingList/src/org/openintents/shopping/ui/PickItemsActivity.java
@@ -8,8 +8,10 @@ import org.openintents.shopping.ui.widget.ShoppingItemsView;
 import android.app.Activity;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v2.os.Build;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.AdapterView.OnItemClickListener;
 
 public class PickItemsActivity extends Activity {
@@ -36,6 +38,19 @@ public class PickItemsActivity extends Activity {
 		// 	}
               // 
 		// });
+		
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.DONUT) {
+			// setting android:onClick in activity_pick_items.xml does not work
+			// yet for Android 1.5
+			Button b = (Button) findViewById(R.id.button1);
+			b.setOnClickListener(new Button.OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					onButton1Click(v);
+				}
+			});
+		}
 	}
 
 	public void onButton1Click(View view) {
